47539c628330b994a5d2adcad1507899f4fe8370
mladen antic <m.antic@elfak.rs>
2024-06-11 15:36:06 +0200

    register login screen boilerplate

---
+++ app/src/main/java/com/example/mobilnibus/RegisterLoginScreen.kt
@@ -0,0 +1,13 @@
+package com.example.mobilnibus
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun RegisterLoginScreen() {
+    Column {
+        Text(text = "Registracija")
+        Text(text = "Prijava")
+    }
+}
