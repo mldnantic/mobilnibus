24edb168ac921d6996ef0d5a796b563768db1c15
mladen antic <m.antic@elfak.rs>
2024-06-12 14:33:28 +0200

    buttons

--- app/src/main/java/com/example/mobilnibus/RegisterLoginScreen.kt
+++ app/src/main/java/com/example/mobilnibus/RegisterLoginScreen.kt
@@ -1,13 +1,30 @@
 package com.example.mobilnibus
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 
 @Composable
 fun RegisterLoginScreen() {
-    Column {
-        Text(text = "Registracija")
-        Text(text = "Prijava")
+    Surface {
+        Column(modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center) {
+            ElevatedButton(onClick = { /*TODO*/ }, modifier = Modifier.fillMaxWidth())
+            {
+                Text(text = "Registracija")
+            }
+            ElevatedButton(onClick = { /*TODO*/ }, modifier = Modifier.fillMaxWidth())
+            {
+                Text(text = "Prijava")
+            }
+        }
     }
 }
